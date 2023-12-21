b09b7f221116807cdf3359c0e08f3427ae38970e
MaximkaProMax <sebu1404@gmail.com>
2023-12-21 12:14:19 +0300

    Last files were added

--- Burykin_PHP/bd/bd.sql
+++ Burykin_PHP/bd/bd.sql
@@ -1,26 +1,116 @@
-CREATE DATABASE IF NOT EXISTS `db`;
-USE `db`;
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Дек 21 2023 г., 12:03
+-- Версия сервера: 8.0.30
+-- Версия PHP: 8.1.9
 
-CREATE TABLE IF NOT EXISTS `users` (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(50) NOT NULL,
-    `email` VARCHAR(100) NOT NULL UNIQUE,
-    `password` VARCHAR(255) NOT NULL,
-    `role` ENUM('admin' ,'teacher', 'student') NOT NULL
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE IF NOT EXISTS `assignments` (
-    `id` INT PRIMARY KEY,
-    `title` VARCHAR(100) NOT NULL,
-    `deadline` DATE NOT NULL,
-    `description` TEXT NOT NULL
-);
 
-CREATE TABLE IF NOT EXISTS `submitted_files` (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `assignment_id` INT NOT NULL,
-    `student_id` INT NOT NULL,
-    `file_path` VARCHAR(255) NOT NULL,
-    `submission_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- База данных: `bd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `assignments`
+--
+
+CREATE TABLE `assignments` (
+  `id` int NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `deadline` date NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `assignments`
+--
+
+INSERT INTO `assignments` (`id`, `title`, `deadline`, `description`) VALUES
+(1, 'asdf', '2023-12-23', 'asdfasdf'),
+(2, 'asdfasdf', '2023-12-22', 'asdfasdf'),
+(3, 'asdfasdf', '2023-12-22', 'asdfasdf'),
+(4, 'asdfasdf', '2023-12-31', 'asdfasdf');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `news`
+--
+
+CREATE TABLE `news` (
+  `id` int NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','teacher','student') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`) VALUES
+(1, 'maximka', 'maximka@gmail.com', '$2y$10$8FwNUuuqsTaD94xamYyQt.AlBvq4YyvbqLuR9pmKV1vtq5bfTO6fi', 'student');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `assignments`
+--
+ALTER TABLE `assignments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `assignments`
+--
+ALTER TABLE `assignments`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
