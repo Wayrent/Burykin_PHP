5755940f7af69db19337b484e48abd187cacd369
MaximkaProMax <sebu1404@gmail.com>
2023-12-21 13:03:22 +0300

    BD HotFix 2

--- Burykin_PHP/bd/bd.sql
+++ Burykin_PHP/bd/bd.sql
@@ -1,26 +1,135 @@
-CREATE DATABASE IF NOT EXISTS `db`;
-USE `db`;
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Дек 21 2023 г., 12:57
+-- Версия сервера: 8.0.30
+-- Версия PHP: 7.2.34
 
-CREATE TABLE IF NOT EXISTS `users` (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(50) NOT NULL,
-    `email` VARCHAR(100) NOT NULL UNIQUE,
-    `password` VARCHAR(255) NOT NULL,
-    `role` ENUM('admin' ,'teacher', 'student') NOT NULL
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE IF NOT EXISTS `assignments` (
-    `id` INT PRIMARY KEY,
-    `title` VARCHAR(100) NOT NULL,
-    `deadline` DATE NOT NULL,
-    `description` TEXT NOT NULL
-);
 
-CREATE TABLE IF NOT EXISTS `submitted_files` (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `assignment_id` INT NOT NULL,
-    `student_id` INT NOT NULL,
-    `file_path` VARCHAR(255) NOT NULL,
-    `submission_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- База данных: `bd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `assignments`
+--
+
+CREATE TABLE `assignments` (
+  `id` int NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `deadline` date NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `assignments`
+--
+
+INSERT INTO `assignments` (`id`, `title`, `deadline`, `description`) VALUES
+(1, 'Pptx', '2023-12-12', 'Создать презентацию'),
+(2, 'Css', '2023-12-12', 'Создать css оформление сайта'),
+(3, 'Html', '2023-12-12', 'Создать структуру сайта'),
+(4, 'docx', '2023-12-14', 'Создать отчёт по работе');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `news`
+--
+
+CREATE TABLE `news` (
+  `id` int NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `news`
+--
+
+INSERT INTO `news` (`id`, `title`, `content`) VALUES
+(1, 'GTA 6', 'Дата выхода 2027');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','teacher','student') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`) VALUES
+(2, 'serega', 'serega@gmail.com', '$2y$10$1LKD3aanWdwE/xjQOIIZCukk79ePU0GCVSvrvoIPxfvEkNkefSgYW', 'student');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `assignments`
+--
+ALTER TABLE `assignments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `assignments`
+--
+ALTER TABLE `assignments`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT для таблицы `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
